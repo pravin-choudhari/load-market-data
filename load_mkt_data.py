f556c7ffc3c5286d0d44db627f14bd1e0d0ab467
Pravin Choudhari <pravinschoudhari@gmail.com>
2023-05-28 14:39:27 +0530

    add readme

--- load_mkt_data.py
+++ load_mkt_data.py
@@ -8,14 +8,9 @@ from datetime import datetime
 dbConn = db()
 dbConn.createTables()
 
-tcs = yf.Ticker("BSLSENETFG.NS")
-data= tcs.history(period="2y")
-print(data)
-index_records1 = data.to_dict(orient='index')
-for index in index_records1:
-  stock = Stock("BSLSENETFG",**index_records1[index])
-  date = index.to_pydatetime()
-  dbConn.insertEntry(date,stock)
+#tcs = yf.Ticker("TCS.NS")
+#data= tcs.history(period="2y")
+#print(data)
 
 scrips = pd.read_csv("nse_scrips.txt")
 f = open("nse_scrips.txt", "r")
@@ -26,10 +21,6 @@ for scrip in f:
   print(f"Going to query: {scrip}")
   scrip_data = y_scrip.history(period="2y")
   
-  #records = scrip_data.to_dict(orient='records')
-  #for row in records:
-  #   stock = Stock(orignal_scrip,**row)
-  #  print(stock)
   index_records = scrip_data.to_dict(orient='index')
   for index in index_records:
     stock = Stock(orignal_scrip,**index_records[index])
